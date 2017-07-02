e3f162dd00ab1ec095f132a230debc438c85e26e
Roi <roi.shabshin@gmail.com>
2017-07-02 11:14:26 +0300

    implemented Devise for Users

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  validates :password, :presence => true,
+  					   :confirmation => true, 
+  					   :length => {:within => 6..40},
+                       :on => :create,
+                       :if => :password, 
+                       :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
+ 
+end
